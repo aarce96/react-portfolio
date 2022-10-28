19ac6152534ec446952544e0f6dcd11d38b92942
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-10-28 14:04:42 -0400

    added function to contact form section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import Nav from './components/Nav';
-import About from './components/About';
-import Gallery from './components/Gallery';
+import React, { useState } from "react";
+import Nav from "./components/Nav";
+import About from "./components/About";
+import Gallery from "./components/Gallery";
+import ContactForm from "./components/Contact";
 
 function App() {
   const [categories] = useState([
@@ -19,8 +20,13 @@ function App() {
 
   return (
     <div>
-      <Nav categories={categories} setCurrentCategory={setCurrentCategory} currentCategory={currentCategory}></Nav>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
