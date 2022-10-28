3b5f90b9260c1804d36ee75bf1a6a4a7820584e3
Arturo Arce <arturo.arce@Arturos-MacBook-Pro.local>
2022-10-28 11:12:51 -0400

    add Gallery section

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from '../../assets/large/projects/my-own-library.png';
 
-function Gallery() {
+function Gallery(props) {
+    const currentCategory = {
+        name: "project",
+        description: "Photo of my own library web application"
+    };
+
     return (
         <section>
-            <h1>NAME</h1>
-            <p>DESCRIPTION</p>
+            <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+            <p>{currentCategory.name}</p>
+            <div className="flex-row">
+                <img src={photo} alt="my own library web application" className="img-thumbnail mx-1" />
+            </div>
         </section>
     );
 }
